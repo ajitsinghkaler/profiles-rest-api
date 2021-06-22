438e94ee34d48862291c4ea72dadf972f14794ab
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-06-22 12:25:53 +0530

    add hello world view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test api View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView Features"""
+        an_apiview = [
+            'Uses HTTP methods as function'
+            'Is similar to a traditional Django view',
+            'Gives you most control over your application Logic',
+            'Is mapped manually to urls'
+        ]
+
+        return Response({
+            'message': 'Hello', 'an_apiview': an_apiview
+        })
